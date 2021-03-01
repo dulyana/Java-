74f4ee3d2593a90442df2f6f3a7dccdb33d0242c
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-03-01 15:01:31 +0530

    Add files via upload

---
+++ com.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slibrary;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author nirma
+ */
+class com {
+
+    static PreparedStatement prepareStatement(String insert_into_CategorycatnamestatusValues) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
